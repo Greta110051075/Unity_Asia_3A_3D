a5b3437384ba1dd52edfe3377145b15477a321e8
Greta110051075 <113588842+Greta110051075@users.noreply.github.com>
2022-12-01 18:39:03 +0800

    12_01

---
+++ Unity_Asia_3A_3D/Assets/Scripts/DialogueData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace greta
+{
+    /// <summary>
+    /// 對話資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "greta/Dialogue Data", fileName = "New Dialogue Data")]
+    public class DialogueData : ScriptableObject
+    {
+        [Header("對話者名稱")]
+        public string dialogueName;
+        [Header("對話內容")]
+        public string[] dialogueContent;
+    }
+}
